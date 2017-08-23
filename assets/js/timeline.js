249128fd26ca30c85eba67db70ac98d43928b262
Pranav Lodha <lodha.pranav2014@gmail.com>
2017-08-23 10:13:30 -0700

    Minor changes to CSS and html for timeline

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -2,54 +2,54 @@ function renderTimeline() {
   // Write all javascript in this part.
   var d = new Date();
   var eventNum = document.getElementById('event1')
-  if(d.Day() == 26 && d.Month() == 8) {
-    eventNum.className="li complete"
-  }
-  var eventNum2 == document.getElementById('event2')
-  if(d.Day() == 4 && d.Month() == 9) {
-    eventNum2.className="li complete"
-    eventNum.className="li complete"
-  }
-  var eventNum3 == document.getElementById('event3')
-  if(d.Day() == 8 && d.Month() == 9) {
-    eventNum.className="li complete"
-    eventNum2.className="li complete"
-    eventNum3.className="li complete"
-  }
-  var eventNum4 == document.getElementById('event4')
-  if(d.Day() == 10 && d.Month() == 9) {
-    eventNum.className="li complete"
-    eventNum2.className="li complete"
-    eventNum3.className="li complete"
-    eventNum4.className="li complete"
-  }
-  var eventNum5 == document.getElementById('event5')
-  if(d.Day() == 13 && d.Month() == 9) {
-    eventNum.className="li complete"
-    eventNum2.className="li complete"
-    eventNum3.className="li complete"
-    eventNum4.className="li complete"
-    eventNum5.className="li complete"
-  }
-  var eventNum6 == document.getElementById('event6')
-  if(d.Day() == 15 && d.Month() == 9) {
-    eventNum.className="li complete"
-    eventNum2.className="li complete"
-    eventNum3.className="li complete"
-    eventNum4.className="li complete"
-    eventNum5.className="li complete"
-    eventNum6.className="li complete"
-  }
-  var eventNum7 == document.getElementById('event7')
-  if(d.Day() == 18 && d.Month() == 9) {
-    eventNum.className="li complete"
-    eventNum2.className="li complete"
-    eventNum3.className="li complete"
-    eventNum4.className="li complete"
-    eventNum5.className="li complete"
-    eventNum6.className="li complete"
-    eventNum7.className="li complete"
-  }
+  // if(d.Day() == 26 && d.Month() == 8) {
+  //   eventNum.className="complete"
+  // }
+  // var eventNum2 == document.getElementById('event2')
+  // if(d.Day() == 4 && d.Month() == 9) {
+  //   eventNum2.className="complete"
+  //   eventNum.className="complete"
+  // }
+  // var eventNum3 == document.getElementById('event3')
+  // if(d.Day() == 8 && d.Month() == 9) {
+  //   eventNum.className="complete"
+  //   eventNum2.className="complete"
+  //   eventNum3.className="complete"
+  // }
+  // var eventNum4 == document.getElementById('event4')
+  // if(d.Day() == 10 && d.Month() == 9) {
+  //   eventNum.className="complete"
+  //   eventNum2.className="complete"
+  //   eventNum3.className="complete"
+  //   eventNum4.className="complete"
+  // }
+  // var eventNum5 == document.getElementById('event5')
+  // if(d.Day() == 13 && d.Month() == 9) {
+  //   eventNum.className="complete"
+  //   eventNum2.className="complete"
+  //   eventNum3.className="complete"
+  //   eventNum4.className="complete"
+  //   eventNum5.className="complete"
+  // }
+  // var eventNum6 == document.getElementById('event6')
+  // if(d.Day() == 15 && d.Month() == 9) {
+  //   eventNum.className="complete"
+  //   eventNum2.className="complete"
+  //   eventNum3.className="complete"
+  //   eventNum4.className="complete"
+  //   eventNum5.className="complete"
+  //   eventNum6.className="complete"
+  // }
+  // var eventNum7 == document.getElementById('event7')
+  // if(d.Day() == 18 && d.Month() == 9) {
+  //   eventNum.className="complete"
+  //   eventNum2.className="complete"
+  //   eventNum3.className="complete"
+  //   eventNum4.className="complete"
+  //   eventNum5.className="complete"
+  //   eventNum6.className="complete"
+  //   eventNum7.className="complete"
+  // }
 }
 
 window.onload = renderTimeline
