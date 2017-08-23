9db15b7f2f23b388f8c46b049a5b98099076ec8d
Pranav Lodha <lodha.pranav2014@gmail.com>
2017-08-23 15:46:16 -0700

    timeline.js allows the slide to point to correct date on timeline

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,55 +1,70 @@
 function renderTimeline() {
-  // Write all javascript in this part.
+
   var d = new Date();
-  var eventNum = document.getElementById('event1')
-  // if(d.Day() == 26 && d.Month() == 8) {
-  //   eventNum.className="complete"
-  // }
-  // var eventNum2 == document.getElementById('event2')
-  // if(d.Day() == 4 && d.Month() == 9) {
-  //   eventNum2.className="complete"
-  //   eventNum.className="complete"
-  // }
-  // var eventNum3 == document.getElementById('event3')
-  // if(d.Day() == 8 && d.Month() == 9) {
-  //   eventNum.className="complete"
-  //   eventNum2.className="complete"
-  //   eventNum3.className="complete"
-  // }
-  // var eventNum4 == document.getElementById('event4')
-  // if(d.Day() == 10 && d.Month() == 9) {
-  //   eventNum.className="complete"
-  //   eventNum2.className="complete"
-  //   eventNum3.className="complete"
-  //   eventNum4.className="complete"
-  // }
-  // var eventNum5 == document.getElementById('event5')
-  // if(d.Day() == 13 && d.Month() == 9) {
-  //   eventNum.className="complete"
-  //   eventNum2.className="complete"
-  //   eventNum3.className="complete"
-  //   eventNum4.className="complete"
-  //   eventNum5.className="complete"
-  // }
-  // var eventNum6 == document.getElementById('event6')
-  // if(d.Day() == 15 && d.Month() == 9) {
-  //   eventNum.className="complete"
-  //   eventNum2.className="complete"
-  //   eventNum3.className="complete"
-  //   eventNum4.className="complete"
-  //   eventNum5.className="complete"
-  //   eventNum6.className="complete"
-  // }
-  // var eventNum7 == document.getElementById('event7')
-  // if(d.Day() == 18 && d.Month() == 9) {
-  //   eventNum.className="complete"
-  //   eventNum2.className="complete"
-  //   eventNum3.className="complete"
-  //   eventNum4.className="complete"
-  //   eventNum5.className="complete"
-  //   eventNum6.className="complete"
-  //   eventNum7.className="complete"
-  // }
+  var d2 = new Date(2017, 9, 20);
+  var diff = 0;
+  var position = 0;
+
+  if(d.getDate() > 20 && d.getDate() <= 26 && d.getMonth() == 9) {
+    diff = d.getDate() - d2.getDate();
+    position = -((1250 - 1167)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else if((d.getDate() > 26 && d.getDate() <= 4) && (d.getMonth() == 9 || d.getMonth() == 10)) {
+    d2 = new Date(2017, 9, 26);
+    diff = d.getDate() - d2.getDate();
+    position = -((1167-1002)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else if(d.getDate() > 4 && d.getDate() <= 8 && d.getMonth() == 10) {
+    d2 = new Date(2017, 10, 4);
+    diff = d.getDate() - d2.getDate();
+    position = -((1002-827)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else if(d.getDate() > 8 && d.getDate() <= 10 && d.getMonth() == 10) {
+    d2 = new Date(2017, 10, 8);
+    diff = d.getDate() - d2.getDate();
+    position = -((827-628)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else if(d.getDate() > 10 && d.getDate() <= 13 && d.getMonth() == 10) {
+    d2 = new Date(2017, 10, 10);
+    diff = d.getDate() - d2.getDate();
+    position = -((628-436)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else if(d.getDate() > 13 && d.getDate() <= 15 && d.getMonth() == 10) {
+    d2 = new Date(2017, 10, 13);
+    diff = d.getDate() - d2.getDate();
+    position = -((436-264)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else if(d.getDate() > 15 && d.getDate() <= 18 && d.getMonth() == 10) {
+    d2 = new Date(2017, 10, 15);
+    diff = d.getDate() - d2.getDate();
+    position = -((264-96)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else if(d.getDate() > 18 && d.getDate() <= 20 && d.getMonth() == 10) {
+    d2 = new Date(2017, 10, 18);
+    diff = d.getDate() - d2.getDate();
+    position = -((96-5)/diff * d.getDate())+"px";
+    document.getElementById("circle").style.left = position;
+  }
+  else {
+    document.getElementById("circle").style.left = "-1250px";
+  }
 }
 
+
+// Line 1: -1250px Start
+// Line 2: -1167px
+// Line 3: -1002px
+// Line 4: -827px
+// Line 5: -628px
+// Line 6: -436px
+// Line 7: -264px
+// Line 8: -96px
+// Line 9: 0px End
 window.onload = renderTimeline
