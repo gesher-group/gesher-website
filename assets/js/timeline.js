5d8e7abc203d28bba2f715e534c06e97738d6e49
Pranav Lodha <lodha.pranav2014@gmail.com>
2017-09-14 18:32:35 -0700

    Timeline javascript rework

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,59 +1,12 @@
 function renderTimeline () {
   var d = new Date()
-  var d2 = new Date(2017, 9, 20)
-  var diff = 0
-  var position = 0
-  if (d.getDate() > 20 && d.getDate() <= 26 && d.getMonth() === 9) {
-    diff = d.getDate() - d2.getDate()
-    position = -((1250 - 1167) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else if ((d.getDate() > 26 && d.getDate() <= 4) && (d.getMonth() === 9 || d.getMonth() === 10)) {
-    d2 = new Date(2017, 9, 26)
-    diff = d.getDate() - d2.getDate()
-    position = -((1167 - 1002) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else if (d.getDate() > 4 && d.getDate() <= 8 && d.getMonth() === 10) {
-    d2 = new Date(2017, 10, 4)
-    diff = d.getDate() - d2.getDate()
-    position = -((1002 - 827) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else if (d.getDate() > 8 && d.getDate() <= 10 && d.getMonth() === 10) {
-    d2 = new Date(2017, 10, 8)
-    diff = d.getDate() - d2.getDate()
-    position = -((827 - 628) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else if (d.getDate() > 10 && d.getDate() <= 13 && d.getMonth() === 10) {
-    d2 = new Date(2017, 10, 10)
-    diff = d.getDate() - d2.getDate()
-    position = -((628 - 436) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else if (d.getDate() > 13 && d.getDate() <= 15 && d.getMonth() === 10) {
-    d2 = new Date(2017, 10, 13)
-    diff = d.getDate() - d2.getDate()
-    position = -((436 - 264) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else if (d.getDate() > 15 && d.getDate() <= 18 && d.getMonth() === 10) {
-    d2 = new Date(2017, 10, 15)
-    diff = d.getDate() - d2.getDate()
-    position = -((264 - 96) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else if (d.getDate() > 18 && d.getDate() <= 20 && d.getMonth() === 10) {
-    d2 = new Date(2017, 10, 18)
-    diff = d.getDate() - d2.getDate()
-    position = -((96 - 5) / diff * d.getDate()) + 'px'
-    document.getElementById('circle').style.left = position
-  } else {
-    document.getElementById('circle').style.left = '-1250px'
-  }
+  if (d.getDate() < 29 && d.getMonth() === 9) { document.getElementById('1').style.borderColor = 'green' }
+  if (d.getDate() === 6 && d.getMonth() === 10) { document.getElementById('2').style.borderColor = 'green' }
+  if (d.getDate() === 7 && d.getMonth() === 10) { document.getElementById('3').style.borderColor = 'green' }
+  if (d.getDate() === 11 && d.getMonth() === 10) { document.getElementById('4').style.borderColor = 'green' }
+  if (d.getDate() === 14 && d.getMonth() === 10) { document.getElementById('5').style.borderColor = 'green' }
+  if (d.getDate() === 17 && d.getMonth() === 10) { document.getElementById('6').style.borderColor = 'green' }
+  if (d.getDate() === 19 && d.getMonth() === 10) { document.getElementById('7').style.borderColor = 'green' }
 };
 
-// Line 1: -1250px Start
-// Line 2: -1167px
-// Line 3: -1002px
-// Line 4: -827px
-// Line 5: -628px
-// Line 6: -436px
-// Line 7: -264px
-// Line 8: -96px
-// Line 9: 0px End
-window.onload = renderTimeline
+window.onload = renderTimeline()
